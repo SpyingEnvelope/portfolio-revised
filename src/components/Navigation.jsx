e3498ed87e8287b70426e2096bebf552d7ce130b
Gad Cooper <gad.cooper@shaw.ca>
2025-10-07 13:34:49 -0600

    fixed navigation menu width. It now does not extend past the body of the website. Also edited the logo to disappear if the width of the screen is less than 360px.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -18,6 +18,7 @@ import logo from "/vite.svg";
 function Navigation() {
   // Visibility state for dealing for the background color of the menu
   const [navBarVisibility, setNavBarVisibility] = useState(true);
+  const [logoVisibility, setLogoVisibility] = useState("visible");
 
   /* The bgColor variable only has a background color if
   navBarVisibility is false*/
@@ -35,22 +36,35 @@ function Navigation() {
     }
   };
 
+  /* This is used to fix an issue where the logo would portrude onto the menu items at screens with
+  a width lower than 361 pixels. The logo's visibility is now set to invisible if the width of the screen
+  is equal to or lower than 360. */
+  window.onresize = function () {
+    if (window.innerWidth <= 360) {
+      setLogoVisibility("invisible");
+    } else {
+      setLogoVisibility("visible");
+    }
+  }
+
   /* The JSX component returned needed to use CSS flex to adjust to screen sizes easily. The justify-end
-  div is used to display the logo while every other menu items is using justify-start.*/
+  div is used to display the logo while every other menu items is using justify-start. REMOVE MAX-WIDTH, xl:left-auto for the nav tag
+  and set w-screen for the logo div to return to the previous settings */
   return (
     <header>
       <nav
-        className={`fixed top-0 left-0  ${bgColor} w-full flex justify-start h-15 items-center z-30`}
+        className={`fixed top-0 left-0 xl:left-auto ${bgColor} w-full flex justify-start h-15 items-center z-30 max-w-[1220px]`}
       >
+        <div className={`flex justify-end flex-row w-full absolute right-5 ${logoVisibility}`}>
+          <a href="#top">
+            <img src={logo} />
+          </a>
+        </div>
         <NavigationMenu>
           <NavigationMenuList>
-            <div className="flex flex-row w-screen justify-start space-x-5 px-5">
+            <div className="flex flex-row w-full justify-start space-x-5 px-3">
               <NavigationMenuItem>
-                <NavigationMenuLink
-                  href="#home"
-                >
-                  Home
-                </NavigationMenuLink>
+                <NavigationMenuLink href="#home">Home</NavigationMenuLink>
               </NavigationMenuItem>
               <NavigationMenuItem>
                 <NavigationMenuLink href="#about">About</NavigationMenuLink>
@@ -63,13 +77,6 @@ function Navigation() {
               <NavigationMenuItem>
                 <NavigationMenuLink href="#footer">Contact</NavigationMenuLink>
               </NavigationMenuItem>
-              <div className="w-screen flex justify-end px-2">
-                <NavigationMenuItem>
-                  <a href="#top">
-                    <img src={logo} />
-                  </a>
-                </NavigationMenuItem>
-              </div>
             </div>
           </NavigationMenuList>
         </NavigationMenu>
