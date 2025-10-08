3dd7a6878c9966ab61bcc1f823767c940f512866
Gad Cooper <gad.cooper@shaw.ca>
2025-10-08 11:54:00 -0600

    Added documentation to the Header component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
+/* This component displays all of the components that are meant to be of the header part of the website.
+It displays the Banner and SlidingInfo components at the moment. */
+
 import Banner from "./Banner";
 import SlidingInfo from "./SlidingInfo";
 
+/* PROPS: None */
 function Header() {
  return <>
     <Banner />
