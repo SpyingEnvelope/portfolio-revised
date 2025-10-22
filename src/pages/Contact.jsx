14f1d4d70d534126ec25a31f8b070d2bfb3cdf5a
Gad Cooper <gad.cooper@shaw.ca>
2025-10-22 10:55:03 -0600

    finished styling contact form. email sending logic and validation logic still necessary

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,6 +7,7 @@ function Contact() {
     const email = formData.get("email");
     const name = formData.get("name");
     const message = formData.get("message");
+    const phone = formData.get("phone")
   }
 
   const [formState, formAction, pending] = useActionState(emailAction, {
@@ -14,12 +15,12 @@ function Contact() {
   });
 
   return (
-    <div className="mt-30 flex flex-col justify-center items-center reg-instrument-sans">
+    <div className="mt-30 w-full flex flex-col justify-center items-center reg-instrument-sans">
       <SectionHeader>CONTACT ME</SectionHeader>
       <h3 className="bold-instrument-sans text-3xl lg:text-4xl tracking-widest">
         WANT TO COLLABORATE? <br /> LET'S GET IN TOUCH!
       </h3>
-      <p className="reg-instrument-sans sm:w-[600px] lg:w-[900px] mt-5 mb-15">
+      <p className="reg-instrument-sans sm:w-[600px] lg:w-[900px] mt-5 mb-10">
         {" "}
         Whether you’re looking to collaborate on a project, have a few
         questions, or just want to say hello, I’d love to hear from you! Feel
@@ -33,13 +34,15 @@ function Contact() {
         <label htmlFor="name" className="mb-3">
           Name
         </label>
-        <motion.input
-          whileFocus={{boxShadow: "0px 0px 5px #3698d5"}}
+        <input
           id="name"
           type="text"
           name="name"
           autoComplete="name"
-          className="block w-130 h-10 bg-[#010412] rounded-md border-1 border-stone-500 text-center mb-3 focus:outline-none"
+          className={`block w-full h-10 md:w-130 bg-[#010412] rounded-md border-1 
+          border-white/50 focus:border-[#3698d5]/50 
+          focus:shadow-sm focus:shadow-[#3698d5] 
+          text-center mb-3 focus:outline-none`}
           required
         />{" "}
         <label htmlFor="company" className="mb-3">
@@ -50,7 +53,10 @@ function Contact() {
           type="text"
           name="company"
           autoComplete="organization"
-          className="block w-130 h-10 bg-[#010412] rounded-md border-1 border-stone-500 text-center mb-3 focus:outline-none"
+          className={`block w-full h-10 md:w-130 h-10 bg-[#010412] rounded-md border-1 
+          border-white/50 focus:border-[#3698d5]/50 
+          focus:shadow-sm focus:shadow-[#3698d5] 
+          text-center mb-3 focus:outline-none`}
         />
         <label htmlFor="email" className="mb-3">
           Email
@@ -60,7 +66,10 @@ function Contact() {
           type="email"
           name="email"
           autoComplete="organization"
-          className="block w-130 h-10 bg-[#010412] rounded-md border-1 border-stone-500 text-center mb-3 focus:outline-none"
+          className={`block w-full h-10 md:w-130 bg-[#010412] rounded-md border-1 
+          border-white/50 focus:border-[#3698d5]/50 
+          focus:shadow-sm focus:shadow-[#3698d5] 
+          text-center mb-3 focus:outline-none`}
           required
         />
         <label htmlFor="phone" className="mb-3">
@@ -71,7 +80,10 @@ function Contact() {
           type="tel"
           name="phone"
           autoComplete="organization"
-          className="block w-130 h-10 bg-[#010412] rounded-md border-1 border-stone-500 text-center mb-3 focus:outline-none"
+          className={`block w-full h-10 md:w-130 bg-[#010412] rounded-md border-1 
+          border-white/50 focus:border-[#3698d5]/50 
+          focus:shadow-sm focus:shadow-[#3698d5] 
+          text-center mb-3 focus:outline-none`}
         />
         <label htmlFor="phone" className="mb-3">
           Message
@@ -81,14 +93,43 @@ function Contact() {
           type="text"
           name="message"
           autoComplete="organization"
-          className="block w-150 h-50 bg-[#010412] rounded-md border-1 border-stone-500 text-center mb-3 focus:outline-none"
+          className={`block w-full md:w-150 h-50 p-2 bg-[#010412] rounded-md border-1 
+          border-white/50 focus:border-[#3698d5]/50 
+          focus:shadow-sm focus:shadow-[#3698d5] 
+          text-left mb-3 focus:outline-none`}
         />
-        <button
+        <motion.button
+          variants={{
+            entry: {
+              backgroundColor: "#010412",
+              boxShadow: "0px 0px 3px #3698D5",
+              border: "1px solid rgba(54, 152, 213, 0.7)",
+              color: "#FFFFFF",
+              scale: 1,
+            },
+            hover: {
+              backgroundColor: "rgba(255, 255, 255, 0.9)",
+              boxShadow: "0px 0px 15px #FFFFFF",
+              border: "1px solid #FFFFFF",
+              color: "#000000",
+              scale: 1,
+            },
+            tap: {
+              backgroundColor: "rgba(255, 255, 255, 0.8)",
+              boxShadow: "0px 0px 15px #FFFFFF",
+              border: "1px solid #FFFFFF",
+              color: "#000000",
+              transition: { type: "spring", duration: 0.1 },
+            },
+          }}
+          initial="entry"
+          whileHover="hover"
+          whileTap="tap"
           type="submit"
-          className="bg-white w-20 rounded-md text-black border-1 border-black cursor-pointer"
+          className="relative overflow-hidden text-white w-40 h-10 rounded-md text-black cursor-pointer"
         >
-          Submit
-        </button>
+          <span>Send Message</span>
+        </motion.button>
       </form>
     </div>
   );
