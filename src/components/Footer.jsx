4b94be80218e85780650b42b23f792a2a3a9867e
SpyingEnvelope <gadi800@gmail.com>
2025-10-24 17:20:35 -0600

    made the earth come up as you scroll down. Changed colours. Please test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,41 +2,40 @@
 and potentially some other links.*/
 import { useRef } from "react";
 import { motion, useInView } from "motion/react";
+import SocialMedia from "./SocialMedia";
 
 function Footer() {
-    const footerRef = useRef();
-    const footerInView = useInView(footerRef);
+  const footerRef = useRef();
+  const footerInView = useInView(footerRef);
 
   return (
-    <motion.footer
-      variants={{ inView: { opacity: 1 }, notInView: { opacity: 1 } }}
-      initial="notInView"
-      whileInView="inView"
-      id="footer"
-      className="mt-30"
-      ref={footerRef}
-    >
+    <motion.footer id="footer" className="mt-30" ref={footerRef}>
       <motion.div
-        transition={{ duration: 2, ease: "easeOut" }}
-        className="w-[500%] h-25 absolute overflow-hidden left-0 h-20"
+
+        className="w-[500%] h-40 absolute overflow-hidden left-0"
       >
-        {footerInView && <motion.div
-          initial={{ boxShadow: "0 0 10px 10px rgba(235, 172, 96, 0.5)" }}
-          animate={{ boxShadow: "0 0 10px 10px rgba(235, 172, 96, 0.3)" }}
-          transition={{
-            repeat: Infinity,
-            duration: 2,
-            repeatType: "reverse",
-            ease: "easeOut",
-          }}
-          className={`absolute -left-[40%] top-5 w-full 
-        aspect-square bg-gradient-to-t from-[#1F39B8] lg:from-99% sm:from-99% 
-        from-98% to-[#8F96C4] lg:to-100% sm:to-100% to-100% rounded-t-full
+        {footerInView && (
+          <motion.div
+            initial={{ y: 80}}
+            animate={{y: 0}}
+            transition={{
+              duration: 0.5,
+              ease: "easeOut",
+            }}
+            className={`absolute -left-[40%] top-5 w-full 
+        aspect-square bg-gradient-to-t from-[#396dbf] lg:from-99% sm:from-99% 
+        from-98% to-[#b6b8ba] lg:to-100% sm:to-100% to-100% rounded-t-full
         opacity-85`}
-          style={{ boxShadow: "0 0 10px 10px rgba(235, 172, 96, 0.5)" }}
-        >
-          {/* <p>&#169; 2025 Gad Cooper. All Rights Reserved.</p> */}
-        </motion.div>}
+            style={{ boxShadow: "0 0 10px 10px rgba(235, 172, 96, 0.5)" }}
+          >
+            {/* <p>&#169; 2025 Gad Cooper. All Rights Reserved.</p> */}
+          </motion.div>
+        )}
+        <div className="w-screen flex justify-center relative top-25">
+          <div className="relative xl:w-[1280px] w-full h-10 ml-10">
+            <SocialMedia />
+          </div>
+        </div>
       </motion.div>
     </motion.footer>
   );
---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,8 @@
+function SocialMedia() {
+    return <div className="flex flex-row space-x-5 reg-instrument-sans">
+        <p>Let's connect!</p>
+        
+    </div>
+}
+
+export default SocialMedia;
