a69b39372032a528290c547e7c3fc9b16557c22f
Gad Cooper <gad.cooper@shaw.ca>
2025-10-27 14:47:17 -0600

    made changes to the error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,8 +2,8 @@ import { NavLink } from "react-router-dom";
 
 function ErrorPage() {
     return <>
-        <h1>Oops! Looks like you went to a dead link.</h1>
-        <NavLink to="/">Click here to go to the home page</NavLink>
+        <h1 className="reg-instrument-sans">Oops! Looks like you went to a dead link.</h1>
+        <NavLink to="/" className="bold-instrument-sans underline text-white hover:text-[#3698d5]">Click here to go to the home page</NavLink>
     </>
 }
 
